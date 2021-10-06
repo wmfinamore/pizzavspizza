e81252860363629efe503f0ba6cf3fc8c77f37b9
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-10-06 16:37:32 -0300

    Implemented pizzeria list API view

--- pizzaproject/stores/views.py
+++ pizzaproject/stores/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import PizzeriaListSerializer
+from .models import Pizzeria
 
-# Create your views here.
+
+class PizzeriaListAPIView(generics.ListAPIView):
+    queryset = Pizzeria.objects.all()
+    serializer_class = PizzeriaListSerializer
