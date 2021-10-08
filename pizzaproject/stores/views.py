512460f33135ec3204fbf201d15daed99b84d15a
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-10-07 22:27:44 -0300

    Implemented pizzeria retrieve API view

--- pizzaproject/stores/views.py
+++ pizzaproject/stores/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .serializers import PizzeriaListSerializer
+from .serializers import PizzeriaListSerializer, PizzeriaDetailSerializer
 from .models import Pizzeria
 
 
 class PizzeriaListAPIView(generics.ListAPIView):
     queryset = Pizzeria.objects.all()
     serializer_class = PizzeriaListSerializer
+
+
+class PizzeriaRetrieveAPIView(generics.RetrieveAPIView):
+    lookup_field = 'id'
+    queryset = Pizzeria.objects.all()
+    serializer_class = PizzeriaDetailSerializer
