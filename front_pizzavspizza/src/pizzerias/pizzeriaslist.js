4931c1de706621890b6eabe3a717762e565aa757
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-10-10 20:30:36 -0300

    Implemented react component

---
+++ front_pizzavspizza/src/pizzerias/pizzeriaslist.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import DummyData from './dummydata.json';
+
+class PizzaList extends Component{
+    render(){
+        return(
+            <h6>Pizza Name Goes Here</h6>
+        )
+    }
+}
+
+export default PizzaList;
