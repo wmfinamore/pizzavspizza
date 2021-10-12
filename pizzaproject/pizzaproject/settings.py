b0e9d7b1f3e5bd85aa79f18c9e10175a7d031297
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-10-11 21:49:41 -0300

    Configured Cors Headers in settings file

--- pizzaproject/pizzaproject/settings.py
+++ pizzaproject/pizzaproject/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +56,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+#CORS_ORIGIN_WHITELIST = []
 
 ROOT_URLCONF = 'pizzaproject.urls'
 
