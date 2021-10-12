89f46642318ff2c232b689b86801d611a764c51b
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-10-12 15:18:47 -0300

    Refactored componentDidMount and render method to use PizzeriaDetail. Refactored pizza Detail class to show pizzeria detail fields from API

--- front_pizzavspizza/src/pizzerias/pizzeriadetail.js
+++ front_pizzavspizza/src/pizzerias/pizzeriadetail.js
@@ -2,14 +2,17 @@ import React, {Component} from "react";
 
 class PizzaDetail extends Component{
     render(){
-    const p = this.props.p
+    const obj = this.props.pizzariaDetail;
         return(
-            <div>
-                <h4>{p.id}</h4>
-                <h4>{p.pizzeria}</h4>
-                <h4>{p.city}</h4>
+            <div style={{ color: "yellow", border: "1px solid yellow" }}>
+                <h4>{obj.pizzeria_name}</h4>
+                <h5>
+                    Adress: {obj.street} {obj.city} {obj.state} {obj.zip_code}
+                </h5>
+                <h6>Phone: {obj.phone_number}</h6>
+                <p>{obj.description}</p>
             </div>
-        )
+        );
     }
 }
 
--- front_pizzavspizza/src/pizzerias/pizzeriaslist.js
+++ front_pizzavspizza/src/pizzerias/pizzeriaslist.js
@@ -1,35 +1,25 @@
 import React, {Component} from "react";
 import axios from "axios";
+import PizzaDetail from "./pizzeriadetail";
 
 class PizzaList extends Component{
-
     constructor(props){
         super(props);
         this.state = {
             pizzeriaDetail: [],
-            pizzeria: "",
+            pizzeria: " ",
             showComponent: false,
         };
         this.getPizzaDetail=this.getPizzaDetail.bind(this);
         this.showPizzeriaDetails=this.showPizzeriaDetails.bind(this);
     }
 
-    componentDidMount(){
-        axios.get("http://127.0.0.1:8000/")
-        .then((response) => {
-            this.setState({pizzeriasData: response.data})
-        })
-        .catch(function (error){
-            console.log(error);
-        })
-    }
-
     getPizzaDetail(item){
         axios.get("http://127.0.0.1:8000".concat(item.absolute_url))
             .then((response) => {
-                this.setState({pizzeria:response.data})
+                this.setState({ pizzeria: response.data })
             })
-            .catch(function(error){
+            .catch(function (error) {
                 console.log(error);
             });
     }
@@ -39,12 +29,28 @@ class PizzaList extends Component{
         this.setState({showComponent: true})
     }
 
+    componentDidMount(){
+        axios
+        .get("http://127.0.0.1:8000/")
+        .then((response) => {
+            this.setState({pizzeriaDetail: response.data});
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
     render(){
         return(
             <div>
-                {this.state.pizzeriasData.map( item => {
-                    return <h3 key={item.id} > {item.pizzeria_name}, {item.city} </h3>
+                {this.state.pizzeriaDetail.map( (item) => {
+                    return <h3 key={item.id} onClick={() => this.showPizzeriaDetails(item)}> 
+                                {item.pizzeria_name}, {item.city} 
+                            </h3>
                 })}
+                {this.state.showComponent ? (
+                    <PizzaDetail pizzariaDetail={this.state.pizzeria} />
+                ) :null}
             </div>
         )
     }
