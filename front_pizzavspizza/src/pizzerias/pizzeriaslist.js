61d5192b8380b930cd33b9af0c9d84d011fe36c6
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-10-11 22:24:56 -0300

    Installed Axios library. Implemented componentDidMount to get data from API

--- front_pizzavspizza/src/pizzerias/pizzeriaslist.js
+++ front_pizzavspizza/src/pizzerias/pizzeriaslist.js
@@ -1,15 +1,28 @@
 import React, {Component} from "react";
-import DummyData from './dummydata.json';
-import PizzaDetail from "./pizzeriadetail";
+import axios from "axios";
 
 class PizzaList extends Component{
+
+    state = {
+        pizzeriasData: [],
+    }
+
+    componentDidMount(){
+        axios.get("http://127.0.0.1:8000/")
+        .then((response) => {
+            this.setState({pizzeriasData: response.data})
+        })
+        .catch(function (error){
+            console.log(error);
+        })
+    }
+
     render(){
         return(
             <div>
-                {DummyData.map(item => {
-                    return <PizzaDetail p={item} />
-                }
-                    )}
+                {this.state.pizzeriasData.map( item => {
+                    return <h3 key={item.id} > {item.pizzeria_name}, {item.city} </h3>
+                })}
             </div>
         )
     }
