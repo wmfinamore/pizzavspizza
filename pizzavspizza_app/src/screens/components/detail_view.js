c12f0ce9902718a9aa799ccc4210090bacc71c87
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-10-24 21:32:30 -0300

    Refactored detail view to use function instead of a class

--- pizzavspizza_app/src/screens/components/detail_view.js
+++ pizzavspizza_app/src/screens/components/detail_view.js
@@ -1,29 +1,26 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
 
-class DetailView extends Component{
-    render(){
-        return (
-            <View style={styles.center}>
-                <Text style={styles.title}>Detail View</Text>
-                <Button 
-                    title="Click for Tabs"
-                    onPress={()=>this.props.navigation.navigate("Tabs")}
-                />
-            </View>
-        );
-    }
+const DetailView = ({ navigation, route }) => {
+    const { objurl } = route.params;
+    const { hey } = route.params;
+    return (
+        <View style={styles.center}>
+            <Text style={styles.title}>{hey}</Text>
+            <Text style={styles.title}>{objurl}</Text>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-    center:{
-        flex:1,
-        justifyContent:"center",
-        alignItems:"center",
+    center: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
     },
-    title:{
-        fontSize:36,
-        marginBottom:16,
+    title: {
+        fontSize: 36,
+        marginBottom: 16,
     }
 })
 
