793eee9dd6e0d1096e1d59061c1ab6f355b2ee23
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-10-29 23:10:08 -0300

    #APP - Implemented text visualization using Text component

--- pizzavspizza_app/src/screens/drawer/screenA.js
+++ pizzavspizza_app/src/screens/drawer/screenA.js
@@ -1,14 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { StyleSheet, Image, SafeAreaView, TextInput, Text } from "react-native";
 
+
 const screenA=()=>{
+    const[pizzeria, setPizzeria] = useState("");
     return(
         <SafeAreaView>
+            <Text 
+                style={{
+                    marginTop: 100,
+                    fontSize: 48,
+                    color: "red",
+                    textAlign: "center",
+                }}
+            >
+                {pizzeria}
+            </Text>
             <TextInput 
                 style={styles.textBox} 
                 autoCapitalize="words"
                 autoCorrect={false}
                 placeholder="Pizzeria"
+                onChangeText={(text)=>setPizzeria(text)}
             />
         </SafeAreaView>
     );
