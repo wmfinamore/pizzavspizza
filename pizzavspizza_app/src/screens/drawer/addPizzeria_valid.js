0318007f95290657baee77016edb56a711751d04
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-11-02 23:05:32 -0300

    #APP - Included addPizzeria_valid to separate validationSchema component

---
+++ pizzavspizza_app/src/screens/drawer/addPizzeria_valid.js
@@ -0,0 +1,8 @@
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+    pizzeria: Yup.string().max(200, 'Must be less than 200 characters').min(3, 'Must be at least 3 characters').required('Required'),
+    city: Yup.string().max(400, 'Must be less than 400 characters').min(3, 'Must be at least 3 characters'),
+})
+
+export default validationSchema;
